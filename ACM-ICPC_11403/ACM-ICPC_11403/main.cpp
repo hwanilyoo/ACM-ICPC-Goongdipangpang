82738507a86c71ca64a64868a65f68eab6e4c179
hwanil <uhi1117@naver.com>
2016-09-13 17:12:49 +0900

    Boj 11403(Floyd_Washall)

---
+++ ACM-ICPC_11403/ACM-ICPC_11403/main.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+
+using namespace std;
+
+#define INF 987654321
+
+int d[101][101];
+int main() {
+	int n;
+	
+	scanf("%d",&n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			scanf("%d",&d[i][j]);
+		}
+	}
+
+
+	for (int k = 1; k <= n; ++k)
+		for (int i = 1; i <= n; ++i)
+			for (int j = 1; j <= n; ++j)
+				if (d[i][k] && d[k][j])
+					d[i][j] = 1;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			printf("%d ", d[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
